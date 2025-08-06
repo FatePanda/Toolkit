5188f37bce748ef823363dab8c36d9b6616dcf56
Fate Panda <walkertheemerald65@gmail.com>
2025-08-06 06:21:31 +0100

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,17 @@
+local Op = _G.SOption
+_G.SOption = nil
+
+if Op then
+    if Op == 1 then
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Keyless-mobile-dex-17888"))()
+    end
+    if Op == 2 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source"))()
+    end
+    if Op == 3 then
+        loadstring(game:HttpGetAsync("https://github.com/richie0866/remote-spy/releases/latest/download/RemoteSpy.lua"))()
+    end
+    if Op == 4 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/depthso/Sigma-Spy/refs/heads/main/Main.lua"))()
+    end
+end
